80c6e0b52656d812733d05c8f3ec7985905292ce
blankspacer155 <monnoi.92@gmail.com>
2020-12-25 16:17:08 +0700

    6-3 complete

--- lab6_3.cpp
+++ lab6_3.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	if(x>=65 and x<=90)
+	{
+		if(x=='A')return'Z';
+		else return x-1;
+	}
+	else return'0';
 }
 
 int main(){
